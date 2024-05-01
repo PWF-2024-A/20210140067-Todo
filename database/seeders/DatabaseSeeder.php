5bc84702f8d2e346e73ed93579630604b25a6c80
Indah <rizqiindah32@gmail.com>
2024-05-01 21:47:33 +0700

    middleware, gate admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,30 +3,33 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 use App\Models\Todo;
-use Illuminate\Support\Str;
-
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
-        User::create(
+        User::factory()->create(
             [
                 'name' => 'Admin',
                 'email' => 'admin@admin.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4KoEa3Ro911C/.og/at2.uheWG/igi',
-                'remember_token' => Str::random(10),
-                'is_admin' => true
+                'is_admin' => true,
             ]
-            );
+        );
+        User::factory()->create(
+            [
+                'name' => 'Rizqi Indah Pratiwi',
+                'email' => 'Rizqi@gmail.com',
+                'is_admin' => true,
+            ]
+        );
 
-            User::factory(100)->create();
-            Todo::factory(500)->create();
+        User::factory(101)->create();
+        Todo::factory(500)->create();
     }
 }
